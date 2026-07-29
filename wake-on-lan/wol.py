4d2a80b050b2d3574cb2d10602666fbc70c60b55
Eric Bell <eric@polymorph.co>
2026-07-28 20:07:22 -0400

    fix: changed port from 9 to 7

--- wake-on-lan/wol.py
+++ wake-on-lan/wol.py
@@ -16,7 +16,7 @@ from pathlib import Path
 TARGET_IP =  "192.168.1.114" # "192.168.1.137"
 TARGET_MAC = "84-47-09-71-CB-6D" # "84:9e:56:07:fb:d3"
 SUBNET_PREFIX = 24
-BROADCAST_PORT = 9
+BROADCAST_PORT = 7 #9
 
 
 def normalize_mac(mac: str) -> bytes:
